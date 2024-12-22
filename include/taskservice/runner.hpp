509688c7d29bc173d43c03a209c5ba874d18e736
darryl west <darryl.west@RainCitySoftware.com>
2024-12-21 17:12:04 -0800

    fix for warning message only on alamo

--- include/taskservice/runner.hpp
+++ include/taskservice/runner.hpp
@@ -15,7 +15,8 @@ namespace taskservice {
     auto exec(const char* cmd) {
         std::array<char, 128> buffer;
         std::string result = "";
-        std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+        
+        std::unique_ptr<FILE, decltype(static_cast<int (*)(FILE*)>(&pclose))> pipe(popen(cmd, "r"), pclose);
 
         if (!pipe) {
             return "the command " + std::string(cmd) + " failed!";
