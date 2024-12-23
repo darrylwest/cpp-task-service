e8a7502de0d134dae693a613d92bd6e1b0bd3383
darryl west <darryl.west@raincitysoftware.com>
2024-12-23 15:29:15 -0800

    version bump

--- src/task-service.cpp
+++ src/task-service.cpp
@@ -3,7 +3,9 @@
 //
 
 #include <spdlog/spdlog.h>
+#include <spdlog/fmt/fmt.h>
 
+#include <vendor/ansi_colors.hpp>
 #include <taskservice/cli.hpp>
 #include <taskservice/logging.hpp>
 #include <taskservice/service.hpp>
@@ -15,8 +17,8 @@
 int main(const int argc, char **argv) {
     using namespace httplib;
 
-    std::cout << argv[0] << std::endl;
     const std::string ss = std::string(argv[0]);
+    fmt::print("{}{}{}\n", colors::yellow, ss, colors::reset);
 
     // run the unit tests
     if (ss.substr(ss.size() - 4) == "unit") {
@@ -27,12 +29,10 @@ int main(const int argc, char **argv) {
     const auto config = taskservice::parse_cli(argc, argv);
 
     std::string version = "Task Runner Service Version: ";
-    auto vers = taskservice::Version();
-
-    version.append(vers.to_string());
 
     if (config.verbose > 0) {
-        spdlog::info("{}", version);
+        auto vers = taskservice::Version().to_string();
+        spdlog::info("Task Service, Version: {}{}{}", colors::cyan, vers, colors::reset);
         spdlog::info("Server Config : {}", config.to_string());
     }
 
